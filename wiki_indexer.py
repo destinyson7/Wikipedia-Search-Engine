6fddd87c077232ae4831b5d85bd0b466b3f9b416
Tanish <tanish.lad@research.iiit.ac.in>
2020-09-06 04:12:53 +0530

    indexing done, running the code overnight to generate index

--- wiki_indexer.py
+++ wiki_indexer.py
@@ -14,11 +14,14 @@ STOP_WORDS.add("cite")
 
 inverted_index = {}
 doc_index = {}
+titles = []
 doc_id = 0
 file_id = 1
 stat_tokens = 0
 
 BLOCK_SIZE = 20000
+INTERMED_SIZE = 10000
+SORT_SIZE = 10000
 
 def postings():
     
@@ -142,36 +145,28 @@ def parse_references(body):
 
     return data
 
-INTERMED_SIZE = 10000
-
 def intermediate_index():
     
     filename = f"intermed{file_id - 1}.txt"
-    fp = open(filename, "w")
+    fp = open(f"{sys.argv[2]}/{filename}", "w")
 
     terms = 0
     temp_cnt = 1
     temp_write = ""
 
-    data = sorted(doc_index.keys())
+    data = sorted(inverted_index.keys())
 
     for token in data:
-        if token not in inverted_index:
-            inverted_index[token] = ""
-        
-        counts = doc_index[token]
-
-        field = f"{token}:D{doc_id}"
-        prefixes = ["t", "i", "b", "c", "l", "r"]
-        for i in range(6):
-            if counts[i] > 0:
-                field += f"{prefixes[i]}{counts[i]}"
-
-        field += "\n"
+        field = f"{token}:{inverted_index[token]}\n"
         temp_write += field
+
         terms += 1
 
         if terms == INTERMED_SIZE * temp_cnt:
+
+            # print("*******\n")
+            # print(temp_write)
+
             fp.write(temp_write)
             temp_write = ""
             temp_cnt += 1
@@ -180,6 +175,13 @@ def intermediate_index():
     temp_write = ""
 
     fp.close()
+
+    fp = open(f"titles/{file_id - 1}.txt", "w")
+    for title in titles:
+        fp.write(f"{title}\n")
+    fp.close()
+
+    print("\n\n\n\n\n\n\n\n\n\n\nINTERMEDIATE INDEX\n\n\n\n\n\n\n\n\n\n\n")
 
 
 class WikiHandler(xml.sax.ContentHandler):
@@ -201,6 +203,7 @@ class WikiHandler(xml.sax.ContentHandler):
         global doc_index
         global file_id
         global inverted_index
+        global titles
 
         if tag == "title":
             self.title = self.current
@@ -217,7 +220,8 @@ class WikiHandler(xml.sax.ContentHandler):
             indexer(preprocess(self.title), 0)
             indexer(preprocess(self.body), 2)
 
-            # print(self.title)
+            print(self.title)
+            titles.append(self.title)
             # print(stat_tokens)
 
             postings()
@@ -236,28 +240,164 @@ class WikiHandler(xml.sax.ContentHandler):
                 intermediate_index()
                 inverted_index = {}
                 file_id += 1
+                titles = []
+        
+        elif tag == "mediawiki":
+            intermediate_index()
+            inverted_index = {}
+            titles = []
+            file_id += 1
 
     def characters(self, content):
         self.current += content
 
-def parse(data):
+def combine(ff, ss):
+    
+    a = open(f"{sys.argv[2]}/intermed{ff}.txt", "r")
+    b = open(f"{sys.argv[2]}/intermed{ss}.txt", "r")
+    c = open(f"{sys.argv[2]}/intermed.txt", "w")
+
+    # new = open(f"{sys.argv[2]}/intermed{ff // 2}.txt", "r")
+
+    aa = a.readline().strip().strip("\n") 
+    bb = b.readline().strip().strip("\n") 
+
+    while aa and bb:
+        atoken = aa.split(":")[0]
+        btoken = bb.split(":")[0]
+
+        if atoken == btoken:
+            c.write(f"{atoken}:{aa.split(':')[1]}{bb.split(':')[1]}\n")
+            aa = a.readline().strip().strip("\n") 
+            bb = b.readline().strip().strip("\n")
+        
+        elif atoken > btoken:
+            c.write(f"{bb}\n") 
+            bb = b.readline().strip().strip("\n")
+        
+        else:
+            c.write(f"{aa}\n")
+            aa = a.readline().strip().strip("\n")
+        
+    while aa:
+        c.write(f"{aa}\n")
+        aa = a.readline().strip().strip("\n") 
+    
+    while bb:
+        c.write(f"{bb}\n") 
+        bb = b.readline().strip().strip("\n")
+    
+    a.close()
+    b.close()
+    c.close()
+    os.remove(f"{sys.argv[2]}/intermed{ff}.txt")
+    os.remove(f"{sys.argv[2]}/intermed{ss}.txt")
+
+    os.rename(f"{sys.argv[2]}/intermed.txt", f"{sys.argv[2]}/intermed{ff // 2}.txt")
+
+def merge():
+
+    print("\n\n\n\n\n\n\n\n\n\n\nMERGING\n\n\n\n\n\n\n\n\n\n\n")
+    
+    file_count = file_id - 1
+
+    while file_count > 1:
+        for i in range(0, file_count, 2):
+            
+            if i + 1 == file_count:
+                old = f"{sys.argv[2]}/intermed{i}.txt"
+                new = f"{sys.argv[2]}/intermed{i // 2}.txt"
+                os.rename(old, new)
+                break
+            
+            combine(i, i + 1)
+        
+        if (file_count & 1):
+            file_count = file_count // 2 + 1
+
+        else:
+            file_count = file_count // 2
+
+def split():
+    fp = open(f"{sys.argv[2]}/intermed0.txt", "r")
+    first_words = open(f"{sys.argv[2]}/bisect.txt", "w")
+
+    a = fp.readline().strip().strip("\n")
+    file_cnt = 0
+    cur_file = ""
+    cur_file_cnt = 0
+    got = False
+    first_word = ""
+
+    while a:
+        cur_file_cnt += 1
+        cur_file += f"{a}\n"
+
+        if not got:
+            first_word = a.split(":")[0]
+            got = True
+
+        a = fp.readline().strip().strip("\n")
+
+        if cur_file_cnt == SORT_SIZE:
+            cur_file_cnt = 0
+            f = open(f"{sys.argv[2]}/{file_cnt}.txt", "w")
+            f.write(cur_file)
+            cur_file = ""
+            f.close()
+            file_cnt += 1
+            got = False
+            first_words.write(f"{first_word}\n")
+        
+    if cur_file_cnt > 0:
+        f = open(f"{sys.argv[2]}/{file_cnt}.txt", "w")
+        f.write(cur_file)
+        f.close()
+        first_words.write(f"{first_word}\n")
+    
+    fp.close()
+    first_words.close()
+    os.remove(f"{sys.argv[2]}/intermed0.txt")
+
+def parse(f):
     parser = xml.sax.make_parser()
     parser.setFeature(xml.sax.handler.feature_namespaces, 0)
     Handler = WikiHandler()
     parser.setContentHandler(Handler)
-    parser.parse(data)
 
-    intermediate_index()
+    # cnt_temp = 0
+
+    for filename in os.listdir(f):
+        print(filename)
+        parser.parse(f"{f}/{filename}")
+        # cnt_temp += 1
+
+        # if cnt_temp == 3:
+        #     break
+
+    
+    # intermediate_index()
 
 if __name__ == "__main__":
-    f = sys.argv[1]
-    parse(f)
-
+    
     if not os.path.exists(sys.argv[2]):
         os.makedirs(sys.argv[2])
 
-    with open(f"{sys.argv[2]}/inverted_index.json", "w") as fp:
-        json.dump(inverted_index, fp)
+    if not os.path.exists("titles"):
+        os.makedirs("titles")
+    
+    f = sys.argv[1]
+    
+    parse(f)    
+    
+    merge()
+    split()
+
+    print(f"total docs: {doc_id}")
+
+
+    # with open(f"{sys.argv[2]}/inverted_index.json", "w") as fp:
+    #     json.dump(inverted_index, fp)
 
     # f = open(sys.argv[3], "w")
     # f.write(f"{stat_tokens}\n{len(inverted_index.keys())}\n")
